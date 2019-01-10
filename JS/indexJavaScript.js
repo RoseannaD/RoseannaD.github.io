e8f55012710f40cbac4b5a60ced19448c7de08ed
RoseannaD <46563699+RoseannaD@users.noreply.github.com>
2019-01-10 13:03:48 +0000

    Version

---
+++ JS/indexJavaScript.js
@@ -0,0 +1,8 @@
+function change() 
+{
+    var elem = document.getElementById("FlipButton");
+    if (elem.value=="S J Tucker") elem.value = "A vanguard of mythpunk";
+    else elem.value = "S J Tucker";
+}
+
+
